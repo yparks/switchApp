28f48b6436551d8b086af9a7656fa76a73459145
arangob <barango@mills.edu>
2017-04-17 18:15:47 -0700

    testing merge

--- app/src/main/java/com/hfad/cs63d/SearchableActivity.java
+++ app/src/main/java/com/hfad/cs63d/SearchableActivity.java
@@ -1,29 +1,12 @@
 package com.hfad.cs63d;
 
-import android.app.ListActivity;
-import android.app.SearchManager;
-import android.content.Intent;
+import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
 
-public class SearchableActivity extends ListActivity {
-
+public class SearchableActivity extends Activity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.search);
-
-        // Get the intent, verify the action and get the query
-        Intent intent = getIntent();
-        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
-            String query = intent.getStringExtra(SearchManager.QUERY);
-            doMySearch(query);
-        }
-    }
-
-    private void doMySearch(String query)
-    {
-        Log.d("Event",query);
     }
 }
