a17f72e4c3217199d197e5164165784fa81dcb56
Roberto AF <mr.amparan@gmail.com>
2017-05-06 16:21:42 -0700

    +Added launcher icons and set action bar icon -Deleted "switch" from the action bar

--- app/src/main/java/edu/mills/cs115/MainActivity.java
+++ app/src/main/java/edu/mills/cs115/MainActivity.java
@@ -47,6 +47,12 @@ public class MainActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Display icon in action bar
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setLogo(R.mipmap.ic_launcher);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
+
         fragmentManager = getSupportFragmentManager();
         //Load HomeFragment once MainActivity Launches
         loadHomeFragment();
